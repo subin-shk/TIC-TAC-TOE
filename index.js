9a158135854a21a4d20119e17da01d3c7f49e6a9
Subin Shakya <subin12.shakya@gmail.com>
2024-04-08 00:05:49 +0545

    line

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const winConditions=[
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8],
+    [2, 5, 8], 
     [0, 4, 8],
     [2, 4, 6]
+];
+const winline=[
+    [1, -110, 0],
+    [0, -30, 0],
+    [0, 45, 0],
+    [80, -30, 90],
+    [-1, -30, 90],
+    [75, -30, 90],
+    [7, -30, 45],
+    [-2, -30, 135]
 ];
 let options=["","","","","","","","",""];
 let currentPlayer="X";
@@ -61,17 +71,21 @@ function checkWinner(){
             continue;
         }
         if(cellA==cellB&&cellB==cellC){
+            drawline();
             roundWon=true;
             break;
+            
         }
-        
         
     }
 
     if(roundWon){
         statusText.textContent=`${currentPlayer} wins!`;
+        document.querySelector(".line").style.width="320px";
+        
+        
+        
         running=false;
-
     }
 
     else if(!options.includes("")){
@@ -83,11 +97,41 @@ function checkWinner(){
     }
 }
 
+function drawline(){
+    let j=0;
+    // for(i=0;i<winline.length;i++){
+        
+        for(j ; j<winline.length; j++){
+    // while(j<winline.length){
+        const e=winline[j];
+        // const l1=options[e[0]];
+        // const l2=options[e[1]];
+        // const l3=options[e[2]];
+        document.querySelector(".line").style.transform=`translate(${e[0]}px,${e[1]}px) rotate(${e[2]}deg)`
+        // j++;
+        break;
+        }
+    // while(j<winline.length){
+    //     const e=winline[j];
+    //             document.querySelector(".line").style.transform=`translate(${e[0]}px,${e[1]}px) rotate(${e[2]}deg)`
+    // // j++;
+    // break;
+    // }
+    // console.log(j);
+    //     j++;
+
+    // }
+
+}
+
+
 function restartGame(){
     currentPlayer="X";
     options=["","","","","","","","",""];
+    document.querySelector(".line").style.width="0px";
     statusText.textContent=`${currentPlayer}'s turn`;
     cells.forEach(cell=>cell.textContent="");
+    
     running=true;
 }
 
@@ -95,16 +139,16 @@ function playercolor(cell, index){
     // cells.forEach(cell=>cell.textContent="");
     
     if(currentPlayer=="X"){
-        for(cellIndex in cells){
+        
             cells.forEach(cellIndex=>cell.style.color = "red");
             // document.querySelector(".cell").style.color = "red";
-        }
+        
     }
     else if(currentPlayer=="O"){
-        for(cellIndex in cells){
+        
             cells.forEach(cellIndex=>cell.style.color = "blue");
             // document.querySelector(".cell").style.color = "blue";
-        }
+        
     }
     
 }
